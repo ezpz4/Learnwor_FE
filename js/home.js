3c1320faa1f6e01cfe59f57c90d0f5b2d85ff62f
yuchaemin2 <kusuri94@gmail.com>
2024-02-19 19:12:50 +0900

    Feat:홈, 로그인, 회원가입 CSS

--- js/home.js
+++ js/home.js
@@ -7,3 +7,20 @@ document.getElementById("signupBtn").addEventListener("click", function() {
     // 클릭 이벤트가 발생했을 때 다른 HTML 페이지로 이동합니다.
     window.location.href = "signup.html";
 });
+
+document.getElementById("mypageBtn").addEventListener("click", function() {
+    // 클릭 이벤트가 발생했을 때 다른 HTML 페이지로 이동합니다.
+    window.location.href = "mypage.html";
+});
+
+function onLoginSuccess() {
+    document.getElementById('beforeLogin').style.display = 'none'; // Hide
+    document.getElementById('afterLogin').style.display = 'block'; // Show
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Example condition: Check if a user is logged in
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+        onLoginSuccess();
+    }
+});
